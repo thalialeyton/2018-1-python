499de9035588c5056cf431909e00ae6ea5ddcc7b
Jorge Alvarado <jalvarador@utec.edu.pe>
2018-04-18 12:33:19 -0500

    Create programa12.py

---
+++ programa12.py
@@ -0,0 +1,11 @@
+x = int(input())
+
+if (x >= 18) and (x <= 65):
+        print("es trabajador")
+elif (x <= 10):
+    print("es niño o niña")
+    print("debe sacar su dni amarillo")
+else:
+    print("no se sabe")
+
+print("fin del program")
