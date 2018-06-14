5722c0612a4b976ecd1fec561db0e32d104326d0
Jorge Alvarado <jalvarador@utec.edu.pe>
2018-06-14 12:58:56 -0500

    Create matrix1.py

---
+++ matrix1.py
@@ -0,0 +1,10 @@
+m = [[2,3, 5], [7, 8, 3], [1, 0, 4]]
+
+print(m[2][1])
+
+michi = [['O', ' ', 'O'], ['X', 'X', 'X'], ['O', ' ', ' ']]
+
+for x in michi:
+    for y in x:
+        print(y, end=' ')
+    print()
